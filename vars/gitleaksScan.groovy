c76dff4d7899755f7c0ef889d89af7d5029393eb
hieunyit <hieuny.it@gmail.com>
2025-09-17 23:29:09 +0700

    Update gitleaksScan.groovy

--- vars/gitleaksScan.groovy
+++ vars/gitleaksScan.groovy
@@ -1,19 +1,9 @@
-import org.jenkinsci.nodesec.ShellUtils
-
 def call(Map cfg = [:]) {
-  runWithCatch('Gitleaks scan secret') {
     String out = cfg.output ?: 'report/gitleaks-report.json'
-
-    String outputDir = ShellUtils.parentDir(out)
-    if (outputDir) {
-      sh "mkdir -p ${ShellUtils.shellQuote(outputDir)}"
-    }
-
     sh """
       gitleaks detect --source . --redact \\
       --report-format json \\
       --gitleaks-ignore-path . \\
       --report-path ${ShellUtils.shellQuote(out)}
     """
-  }
 }
