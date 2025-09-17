981322db07651cc6ff4ccb21754dd51a5ed8f4f2
hieunyit <hieuny.it@gmail.com>
2025-09-18 00:06:30 +0700

    Update npmAudit.groovy

--- vars/npmAudit.groovy
+++ vars/npmAudit.groovy
@@ -1,23 +1,22 @@
 def call(Object arg = null) {
-    String out
-    if (arg instanceof CharSequence) {
-      out = arg.toString().trim()
-    } else if (arg instanceof Map) {
-      out = (arg.output as String)?.trim()
-    }
-    if (!out) {
-      String base = (env.REPORT_DIR ?: 'report').trim()
-      out = "${base}/npm-audit-report.json"
-    }
-    def parentDir = { String p ->
-      int i = (p ?: '').lastIndexOf('/')
-      i > 0 ? p.substring(0, i) : ''
-    }
-    String dir = parentDir(out)
-    if (dir) {
-      sh "mkdir -p ${shQ(dir)}"
-    }
-
-    sh "npm audit --audit-level=high --json > ${shQ(out)}"
+  String out
+  if (arg instanceof CharSequence) {
+    out = arg.toString().trim()
+  } else if (arg instanceof Map) {
+    out = (arg.output as String)?.trim()
   }
+  if (!out) {
+    String base = (env.REPORT_DIR ?: 'report').trim()
+    out = "${base}/npm-audit-report.json"
+  }
+  def parentDir = { String p ->
+    int i = (p ?: '').lastIndexOf('/')
+    i > 0 ? p.substring(0, i) : ''
+  }
+  String dir = parentDir(out)
+  if (dir) {
+    sh "mkdir -p ${shQ(dir)}"
+  }
+  
+  sh "npm audit --audit-level=high --json > ${shQ(out)}"
 }
