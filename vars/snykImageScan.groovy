2fa982ae8bdd5aaa2f39cdcca5845e6c18e18361
hieunyit <hieuny.it@gmail.com>
2025-09-17 22:35:25 +0700

    Fix security scan utilities

--- vars/snykImageScan.groovy
+++ vars/snykImageScan.groovy
@@ -1,10 +1,12 @@
 def call(Map cfg = [:]) { runWithCatch('Snyk scan image') {
   String out = cfg.output ?: 'report/snyk-image.json'
   String dockerfile = cfg.dockerfile ?: 'Dockerfile'
+  String awkFile = '.jenkins-awk-extract-image.awk'
+  writeFile file: awkFile, text: libraryResource('utils/awk-extract-image.awk')
   withCredentials([string(credentialsId: cfg.tokenCredId ?: 'snyk', variable: 'SNYK_TOKEN')]) {
       sh """
         mkdir -p report
-        dockerImageName=\$( ${libraryResource 'utils/awk-extract-image.awk'} ${dockerfile} )
+        dockerImageName=\$(awk -f ${awkFile} ${dockerfile})
         snyk container test --severity-threshold=high --json-file-output=${out} \${dockerImageName}
       """
     }
--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,9 +1,11 @@
 def call(Map cfg = [:]) { runWithCatch('Trivy scan') {
   String out = cfg.output ?: 'report/trivy-report.json'
   String dockerfile = cfg.dockerfile ?: 'Dockerfile'
-    sh """
+  String awkFile = '.jenkins-awk-extract-image.awk'
+  writeFile file: awkFile, text: libraryResource('utils/awk-extract-image.awk')
+  sh """
       mkdir -p report
-      dockerImageName=\$( ${libraryResource 'utils/awk-extract-image.awk'} ${dockerfile} )
+      dockerImageName=\$(awk -f ${awkFile} ${dockerfile})
       trivy image --scanners vuln --severity HIGH,CRITICAL --exit-code 1 --no-progress --quiet -f json -o ${out} \${dockerImageName}
     """
   }
