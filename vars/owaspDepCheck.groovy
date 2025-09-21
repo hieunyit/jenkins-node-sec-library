6e8ea96bffe5d2a708ca7e423075e41af2e066b1
hieunyit <hieuny.it@gmail.com>
2025-09-21 21:09:32 +0700

    Update owaspDepCheck.groovy

--- vars/owaspDepCheck.groovy
+++ vars/owaspDepCheck.groovy
@@ -1,15 +1,9 @@
-def call(Map cfg = [:]) {
-    runWithCatch('OWASP Dependency Check') {
-        String reportDir = cfg.reportDir ?: 'report'
-        String odcInst = cfg.odcInstallation ?: 'OWASP-DepCheck-12'
-        
-        dependencyCheck additionalArguments: """
-            --scan './
-            --out './${reportDir}'
-            --format 'ALL'
-            --exclude '**/test/files/**'
-            --disableArchive
-            --prettyPrint
-        """.stripIndent().trim(), odcInstallation: odcInst
-    }
+def call(String output = null) {
+  String reportDir = output ?: "${env.REPORT_DIR ?: 'report'}"
+  String installation = 'OWASP-DepCheck-12'
+  sh 'mkdir -p ${reportDir}'
+  dependencyCheck(
+    additionalArguments: "--scan ./ --out ./${reportDir} --format ALL --exclude '**/test/files/**' --disableArchive --prettyPrint",
+    odcInstallation: installation
+  )
 }
