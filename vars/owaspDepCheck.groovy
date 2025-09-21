63613813cd70364ee8048b18d496cc7ae39281af
hieunyit <hieuny.it@gmail.com>
2025-09-21 21:15:16 +0700

    Update owaspDepCheck.groovy

--- vars/owaspDepCheck.groovy
+++ vars/owaspDepCheck.groovy
@@ -1,7 +1,7 @@
 def call(String output = null) {
   String reportDir = output ?: "${env.REPORT_DIR ?: 'report'}"
   String installation = 'OWASP-DepCheck-12'
-  sh 'mkdir -p ${reportDir}'
+  sh "mkdir -p ${reportDir}"
   dependencyCheck(
     additionalArguments: "--scan ./ --out ./${reportDir} --format ALL --exclude '**/test/files/**' --disableArchive --prettyPrint",
     odcInstallation: installation
