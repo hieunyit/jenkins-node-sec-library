646d4793fd3af13b3ce7f0390162f855b6c33eb1
hieunyit <hieuny.it@gmail.com>
2025-09-21 20:51:59 +0700

    Update npmAudit.groovy

--- vars/npmAudit.groovy
+++ vars/npmAudit.groovy
@@ -1,13 +1,4 @@
-def call(Object arg = null) {
-  String out
-  if (arg instanceof CharSequence) {
-    out = (arg as String)?.trim()
-  } else if (arg instanceof Map) {
-    out = (arg.output as String)?.trim()
-  }
-  if (!out) {
-    String base = (env.REPORT_DIR ?: 'report').trim()
-    out = "${base}/npm-audit-report.json"
-  }
-  sh "npm audit --audit-level=critical --json > ${out}"
+def call(String output = null) {
+    String outFile = output ?: "${env.REPORT_DIR ?: 'report'}/npm-audit-report.json"
+    sh "npm audit --audit-level=critical --json > ${outFile}"
 }
