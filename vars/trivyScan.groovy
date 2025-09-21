a0911d847d1120fa4a9f39249f9cfc13ed554c1c
hieunyit <hieuny.it@gmail.com>
2025-09-21 21:37:57 +0700

    Update trivyScan.groovy

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,25 +1,16 @@
-import org.jenkinsci.nodesec.ShellUtils
-
-def call(Map cfg = [:]) {
-  runWithCatch('Trivy scan') {
-    String out = cfg.output ?: 'report/trivy-report.json'
-    String dockerfile = cfg.dockerfile ?: 'Dockerfile'
+def call(String output = null, String dockerfile = 'Dockerfile') {
+    String outFile = output ?: "${env.REPORT_DIR ?: 'report'}/trivy-report.json"
     String awkFile = '.jenkins-awk-extract-image.awk'
-
-    String outputDir = ShellUtils.parentDir(out)
-    if (outputDir) {
-      sh "mkdir -p ${ShellUtils.shellQuote(outputDir)}"
-    }
-
+    
+    sh "mkdir -p \"\$(dirname '${outFile}')\""
+    
     writeFile file: awkFile, text: libraryResource('utils/awk-extract-image.awk')
-
     try {
-      sh """
-        dockerImageName=\$(awk -f ${ShellUtils.shellQuote(awkFile)} ${ShellUtils.shellQuote(dockerfile)})
-        trivy image --scanners vuln --severity HIGH,CRITICAL --exit-code 1 --no-progress --quiet -f json -o ${ShellUtils.shellQuote(out)} "\${dockerImageName}"
-      """
+        sh """
+            dockerImageName=\$(awk -f '${awkFile}' '${dockerfile}')
+            trivy image --scanners vuln --severity HIGH,CRITICAL --exit-code 1 --no-progress --quiet -f json -o '${outFile}' "\${dockerImageName}"
+        """
     } finally {
-      sh "rm -f ${ShellUtils.shellQuote(awkFile)}"
+        sh "rm -f '${awkFile}'"
     }
-  }
 }
