56b4746542dee72f42282cfaf2459ee29ed11c98
hieunyit <hieuny.it@gmail.com>
2025-09-21 21:26:42 +0700

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,11 +1,12 @@
-def call(Map cfg = [:]) {
-  String projectKey = cfg.projectKey ?: 'juice-shop'
-  String exclusions = cfg.exclusions ?: '**/test/**'
-  withSonarQubeEnv(cfg.server ?: 'SonarQube Server') {
-    sh '''
-      ${SONAR_SCANNER_HOME}/bin/sonar-scanner \
-      -Dsonar.projectKey=${projectKey} \
-      -Dsonar.exclusions=${exclusions}
-    '''
-  }
+def call(String projectKey = null, String server = 'SonarQube Server') {
+    String project = projectKey ?: 'juice-shop'
+    String exclusions = '**/test/**'
+    
+    withSonarQubeEnv(server) {
+        sh """
+            \${SONAR_SCANNER_HOME}/bin/sonar-scanner \\
+              -Dsonar.projectKey=${project} \\
+              -Dsonar.exclusions=${exclusions}
+        """
+    }
 }
