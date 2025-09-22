d65cbba73621d3df68030bb12af8a10d424620ed
hieunyit <hieuny.it@gmail.com>
2025-09-22 09:09:42 +0700

    Update cosignSign.groovy

--- vars/cosignSign.groovy
+++ vars/cosignSign.groovy
@@ -1,4 +1,18 @@
-def call(String digestFile = 'digest.txt', String imageName = 'docker.io/hieuny/juice-shop', String keyCredId = 'cosign-private-key', String passCredId = 'cosign-pass') {
+def call(Object arg = null) {
+    String digestFile = 'digest.txt'
+    String imageName = 'docker.io/hieuny/juice-shop'
+    String keyCredId = 'cosign-private-key'
+    String passCredId = 'cosign-pass'
+    
+    if (arg instanceof CharSequence) {
+        imageName = arg.toString().trim()
+    } else if (arg instanceof Map) {
+        digestFile = arg.digestFile ?: 'digest.txt'
+        imageName = arg.imageName ?: 'docker.io/hieuny/juice-shop'
+        keyCredId = arg.keyCredId ?: 'cosign-private-key'
+        passCredId = arg.passCredId ?: 'cosign-pass'
+    }
+    
     withCredentials([
         file(credentialsId: keyCredId, variable: 'COSIGN_KEY'),
         string(credentialsId: passCredId, variable: 'COSIGN_PASSWORD')
