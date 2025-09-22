a07630758e5bc2d2aa1066e931a085e36b51868f
hieunyit <hieuny.it@gmail.com>
2025-09-22 20:15:29 +0700

    Update npmAudit.groovy

--- vars/npmAudit.groovy
+++ vars/npmAudit.groovy
@@ -1,4 +1,7 @@
 def call(String output = null) {
-    String outFile = output ?: "${env.REPORT_DIR ?: 'report'}/npm-audit-report.json"
-    sh "npm audit --audit-level=critical --json > ${outFile}"
+  String outFile = output ?: "${env.REPORT_DIR ?: 'report'}/npm-audit-report.json"
+  sh """
+    mkdir -p "\$(dirname '${outFile}')"
+    npm audit --audit-level=critical --json > ${outFile}
+  """
 }
