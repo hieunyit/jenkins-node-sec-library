4633c7b2153f9cfbec5dc16b0dc56c8c994e3efa
hieunyit <hieuny.it@gmail.com>
2025-09-22 20:15:50 +0700

    Update retireJsScan.groovy

--- vars/retireJsScan.groovy
+++ vars/retireJsScan.groovy
@@ -1,8 +1,7 @@
 def call(String output = null) {
-    String outFile = output ?: "${env.REPORT_DIR ?: 'report'}/retire-report.json"
-    
-    sh """
-        mkdir -p "\$(dirname '${outFile}')"
-        retire --severity high --path . --outputformat json --outputpath '${outFile}'
-    """
+  String outFile = output ?: "${env.REPORT_DIR ?: 'report'}/retire-report.json"
+  sh """
+    mkdir -p "\$(dirname '${outFile}')"
+    retire --severity high --path . --outputformat json --outputpath '${outFile}'
+  """
 }
