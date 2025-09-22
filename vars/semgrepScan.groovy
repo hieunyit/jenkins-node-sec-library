9dfc7aa70f1d555cd33b0e3667754c79b6f1bf65
hieunyit <hieuny.it@gmail.com>
2025-09-22 20:16:19 +0700

    Update semgrepScan.groovy

--- vars/semgrepScan.groovy
+++ vars/semgrepScan.groovy
@@ -1,17 +1,16 @@
 def call(String output = null) {
-    String outFile = output ?: "${env.REPORT_DIR ?: 'report'}/semgrep-report.json"
-    
-    sh """
-        mkdir -p "\$(dirname '${outFile}')"
-        semgrep scan \\
-          --config p/owasp-top-ten \\
-          --config p/security-audit \\
-          --config p/secrets \\
-          --config p/javascript \\
-          --metrics=off \\
-          --exclude node_modules --exclude dist --exclude build --exclude coverage --exclude .git \\
-          --timeout 10 \\
-          --error \\
-          --json --json-output='${outFile}'
-    """
+  String outFile = output ?: "${env.REPORT_DIR ?: 'report'}/semgrep-report.json"
+  sh """
+    mkdir -p "\$(dirname '${outFile}')"
+    semgrep scan \\
+      --config p/owasp-top-ten \\
+      --config p/security-audit \\
+      --config p/secrets \\
+      --config p/javascript \\
+      --metrics=off \\
+      --exclude node_modules --exclude dist --exclude build --exclude coverage --exclude .git \\
+      --timeout 10 \\
+      --error \\
+      --json --json-output='${outFile}'
+  """
 }
