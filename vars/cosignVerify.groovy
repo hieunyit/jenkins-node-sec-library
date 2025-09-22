bb53af22d8d995a8c349fea5b14f2bc35152352e
hieunyit <hieuny.it@gmail.com>
2025-09-22 14:37:53 +0700

    Update cosignVerify.groovy

--- vars/cosignVerify.groovy
+++ vars/cosignVerify.groovy
@@ -1,12 +1,12 @@
 def call(Object arg = null) {
     String keyCredId = 'cosign-public-key'
-    String imageTag = '${env.GIT_COMMIT}'
+    String imageTag = '${env.VERSION}'
     
     if (arg instanceof CharSequence) {
         imageTag = arg.toString().trim()
     } else if (arg instanceof Map) {
         keyCredId = arg.keyCredId ?: 'cosign-public-key'
-        imageTag = arg.imageTag ?: '${env.GIT_COMMIT}'
+        imageTag = arg.imageTag ?: '${env.VERSION}'
     }
     
     withCredentials([file(credentialsId: keyCredId, variable: 'COSIGN_PUBLIC_KEY')]) {
